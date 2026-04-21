3b2ca545d14eb2057b1d3de72b362dd5a88d0851
Noaht8 <noahtsegay8@gmail.com>
2026-04-21 16:47:45 +0300

    System Prompt

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from prompts import system_prompt
+
 
 def main():
     parser = argparse.ArgumentParser(description="AI Assistant")
@@ -26,6 +28,7 @@ def main():
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
     
     if response.usage_metadata is None:
