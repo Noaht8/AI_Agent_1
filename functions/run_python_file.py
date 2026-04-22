3f4f6babbf1091de69e404a49fdcb818aa3ea9f5
Noaht8 <noahtsegay8@gmail.com>
2026-04-22 11:53:20 +0300

    Declarations for run_python_file.py

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -44,3 +44,16 @@ def run_python_file(working_directory, file_path, args=None):
         return f"Error: executing Python file: {e}"
 
 
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Run python file in a specified directory relative to the working directory",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="Directory path to list files from, relative to the working directory (default is the working directory itself)",
+            ),
+        },
+    ),
+)
