764db6deab3649a4070b801feb6bf960be1b132a
Noaht8 <noahtsegay8@gmail.com>
2026-04-25 09:40:15 +0300

    Agent Loop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import sys
 
 from dotenv import load_dotenv
 from google import genai
@@ -26,44 +27,62 @@ def main():
     
 
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
-    )
-    
-    if response.usage_metadata is None:
-        raise RuntimeError(
-            "Failed to get usage metadata from Gemini API response. "
-            "This usually indicates the request failed or was blocked."
-        )
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    print("Response:")
-    function_results = []
+    for _ in range(20):
 
-    if response.function_calls != None:
-        for function_call in response.function_calls:
-            function_call_result = call_function(function_call, args.verbose)
-            if function_call_result.parts == []:
-                raise Exception("types.Content object is Empty")
-            if function_call_result.parts[0].function_response == None:
-                raise Exception("Function Response Property is None")
-            if function_call_result.parts[0].function_response.response == None:
-                raise Exception("None Response")
+        try:
 
+            response = client.models.generate_content(
+                model="gemini-2.5-flash",
+                contents=messages,
+                config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
+            )
 
-            function_results.append(function_call_result.parts[0])
+            if response.candidates:
+                for candidate in response.candidates:
+                    if candidate.content:
+                        messages.append(candidate.content)
+            
+            if response.usage_metadata is None:
+                raise RuntimeError(
+                    "Failed to get usage metadata from Gemini API response. "
+                    "This usually indicates the request failed or was blocked."
+                )
             if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
+                print(f"User prompt: {args.user_prompt}")
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+            print("Response:")
+            function_results = []
+
+            if response.function_calls != None:
+                for function_call in response.function_calls:
+                    function_call_result = call_function(function_call, args.verbose)
+                    if function_call_result.parts == []:
+                        raise Exception("types.Content object is Empty")
+                    if function_call_result.parts[0].function_response == None:
+                        raise Exception("Function Response Property is None")
+                    if function_call_result.parts[0].function_response.response == None:
+                        raise Exception("None Response")
 
 
-            # print(f"Calling function: {function_call.name}({function_call.args})")
-    else:
-        print(response.text)
+                    function_results.append(function_call_result.parts[0])
+                    if args.verbose:
+                        print(f"-> {function_call_result.parts[0].function_response.response}")
+                    
+                messages.append(types.Content(role="user", parts=function_results))
+
+                    # print(f"Calling function: {function_call.name}({function_call.args})")
+            else:
+                print(response.text)
+                return
+
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
+        
+        print(f"Maximum iterations reached")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
